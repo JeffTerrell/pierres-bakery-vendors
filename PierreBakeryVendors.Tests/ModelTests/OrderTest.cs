babe038b61fc3fc9f2e4ab28abf014790fb98e0e
Jeff Terrell <jeffaterrell@gmail.com>
2021-12-17 15:28:48 -0500

    add failing logic for Order-test2(return order description)

--- PierreBakeryVendors.Tests/ModelTests/OrderTest.cs
+++ PierreBakeryVendors.Tests/ModelTests/OrderTest.cs
@@ -13,13 +13,27 @@ namespace PierreBakeryVendors.Tests
     {
       //Arrange
       string orderTitle = "Test Order";
-      Order newOrder = new Order(orderTitle);
+      Order newOrder = new Order(orderTitle, "Order Description");
 
       //Act
       string result = newOrder.orderTitle;
 
       //Assert
       Assert.AreEqual(orderTitle, result);
+    }
+
+    [TestMethod]
+    public void GetOrderDescription_ReturnsOrderDescription_String()
+    {
+      //Arrange
+      string orderDescription = "Test Description";
+      Order newOrder = new Order("Order Title", orderDescription);
+
+      //Act
+      string result = newOrder.orderDescription;
+
+      //Assert
+      Assert.AreEqual(orderDescription, result);
     }
   }  
 }
--- PierreBakeryVendors/Models/Order.cs
+++ PierreBakeryVendors/Models/Order.cs
@@ -2,11 +2,13 @@ namespace PierreBakeryVendors.Models
 {
   public class Order
   {
-    public string orderTitle { get; set;}
+    public string orderTitle { get; set; }
+    public string orderDescription { get; set; }
 
-    public Order(string title)
+    public Order(string title, string description)
     {
       orderTitle = title;
+      orderDescription = "test";
     }
   }
 }
