c343d350fa40b42a4e36e8e89be6b7cf1b9cbf15
Jeff Terrell <jeffaterrell@gmail.com>
2021-12-17 15:51:40 -0500

    add failing logic for Order-test4(return order price)

--- PierreBakeryVendors.Tests/ModelTests/OrderTest.cs
+++ PierreBakeryVendors.Tests/ModelTests/OrderTest.cs
@@ -13,7 +13,7 @@ namespace PierreBakeryVendors.Tests
     {
       //Arrange
       string orderTitle = "Test Order";
-      Order newOrder = new Order(orderTitle, "Order Description", "01/01/2011");
+      Order newOrder = new Order(orderTitle, "Order Description", "01/01/2011", 1);
 
       //Act
       string result = newOrder.orderTitle;
@@ -27,7 +27,7 @@ namespace PierreBakeryVendors.Tests
     {
       //Arrange
       string orderDescription = "Test Description";
-      Order newOrder = new Order("Order Title", orderDescription, "01/01/2011");
+      Order newOrder = new Order("Order Title", orderDescription, "01/01/2011", 1);
 
       //Act
       string result = newOrder.orderDescription;
@@ -41,13 +41,27 @@ namespace PierreBakeryVendors.Tests
     {
       //Arrange
       string orderDate = "07/20/2021";
-      Order newOrder = new Order("Order Title", "Order Description", orderDate);
+      Order newOrder = new Order("Order Title", "Order Description", orderDate, 1);
 
       //Act
       string result = newOrder.orderDate;
 
       //Assert
       Assert.AreEqual(orderDate, result);
+    }
+
+    [TestMethod]
+    public void GetOrderPrice_ReturnsOrderPrice_Int()
+    {
+      //Arrange
+      int orderPrice = 100;
+      Order newOrder = new Order("Order Title", "Order Description", "01/01/2011", orderPrice);
+
+      //Act
+      int result = newOrder.orderPrice;
+
+      //Assert
+      Assert.AreEqual(orderPrice, result);
     }
   }  
 }
--- PierreBakeryVendors/Models/Order.cs
+++ PierreBakeryVendors/Models/Order.cs
@@ -7,14 +7,15 @@ namespace PierreBakeryVendors.Models
   {
     public string orderTitle { get; set; }
     public string orderDescription { get; set; }
-
     public string orderDate { get; set; }
+    public int orderPrice { get; set; }
 
-    public Order(string title, string description, string date)
+    public Order(string title, string description, string date, int price)
     {
       orderTitle = title;
       orderDescription = description;
       orderDate = date;
+      orderPrice = 50;
     }
   }
 }
