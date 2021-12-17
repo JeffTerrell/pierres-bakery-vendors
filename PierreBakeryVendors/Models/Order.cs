34ec8cd76cf0fc31f848d9c301e1601933c39608
Jeff Terrell <jeffaterrell@gmail.com>
2021-12-17 15:31:19 -0500

    add passing logic for Order-test2(return order description)

--- PierreBakeryVendors/Models/Order.cs
+++ PierreBakeryVendors/Models/Order.cs
@@ -8,7 +8,7 @@ namespace PierreBakeryVendors.Models
     public Order(string title, string description)
     {
       orderTitle = title;
-      orderDescription = "test";
+      orderDescription = description;
     }
   }
 }
