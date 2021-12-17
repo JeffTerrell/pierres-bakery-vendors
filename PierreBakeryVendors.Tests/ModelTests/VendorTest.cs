fe2d6b04d84e80485d773bf0874ecff401a7bb16
Jeff Terrell <jeffaterrell@gmail.com>
2021-12-17 13:35:12 -0500

    add failing logic for Vendor-test3(all objects in list)

--- PierreBakeryVendors.Tests/ModelTests/VendorTest.cs
+++ PierreBakeryVendors.Tests/ModelTests/VendorTest.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using PierreBakeryVendors.Models;
 using System;
 
@@ -34,6 +35,34 @@ namespace ToDoList.Tests
 
       //Assert
       Assert.AreEqual(vendorDesciption, result);
+    }
+
+    [TestMethod]
+    public void GetVendorList_ReturnsAllVendorObjectsInList_VendorList()
+    {
+      //Arrange
+      string vendorNameOne = "Homer's Dohnuts";
+      string vendorDescriptionOne = "Simpsons themed donut shop, also sells coffee";
+      string vendorNameTwo = "Java's Ark Coffee House";
+      string vendorDescriptionTwo = "Large coffee shop with multiple locations, also sells muffins and pastries";
+      Vendor newVendor1 = new Vendor(vendorNameOne, vendorDescriptionOne);
+      Vendor newVendor2 = new Vendor(vendorNameTwo, vendorDescriptionTwo);
+      List<Vendor> newVendorList = new List<Vendor> { newVendor1, newVendor2 };
+      foreach (Vendor thisVendor in newVendorList)
+      {
+        Console.WriteLine("Output result Name: " + thisVendor.vendorName);
+        Console.WriteLine("Output result Description: " + thisVendor.vendorDescription);
+      }
+      //Act
+      List<Vendor> result = Vendor.GetVendorList();
+      foreach (Vendor thisVendor in result)
+      {
+        Console.WriteLine("Output result Name: " + thisVendor.vendorName);
+        Console.WriteLine("Output result Description: " + thisVendor.vendorDescription);
+      }
+
+      //Assert
+      CollectionAssert.AreEqual(newVendorList, result);
     }
   }
 }    
--- PierreBakeryVendors/Models/Vendor.cs
+++ PierreBakeryVendors/Models/Vendor.cs
@@ -1,15 +1,24 @@
+using System.Collections.Generic;
+
 namespace PierreBakeryVendors.Models
 {
 
   public class Vendor
   {
-
+    private static List<Vendor> _vendorList = new List<Vendor> {};
     public string vendorName { get; set; }
     public string vendorDescription { get; set; }
+    
     public Vendor(string name, string description)
     {
       vendorName = name;
       vendorDescription = description;
+      _vendorList.Add(this);
+    }
+
+    public static List<Vendor> GetVendorList()
+    {
+      return _vendorList;
     }
   }
 }
